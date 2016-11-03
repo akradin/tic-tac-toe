73c0da0d9da499504eecfcb386e2941d4499a3e3
akradin <alex.kradin@gmail.com>
2016-11-03 09:38:57 -0400

    Get check win function to work
    Disable clicks after win condition is met

--- assets/scripts/clickhandlers.js
+++ assets/scripts/clickhandlers.js
@@ -3,11 +3,6 @@ const logic = require('./logic');
 
 let board = ['', '', '', '', '', '', '', '', ''];
 
-let x = function(){
-  x = logic.y * 2;
-  console.log(x);
-};
-
 let player_switch = 'x';
 
 let player_turn = function(){
@@ -24,7 +19,7 @@ let check_game = function(){
     let new_text = $(value).text();
     let i = index;
     board.splice(i, 1, new_text);
-    console.log(board);
+
 
 
   });
@@ -36,31 +31,31 @@ let change_box= function(){
     $(this).css('background-color','black');
     $(this).text('X');
     player_turn();
-    console.log('x');
   }
   else{
     $(this).css('background-color','blue');
     $(this).text('Y');
     player_turn();
-    console.log('y');
   }
   check_game();
+  console.log(board);
 
-  //logic.check_win(board);
+  logic.check_win(board);
 };
 
+$('.content').one("click", change_box);
 
-$('.box-0').one("click", change_box);
-$('.box-1').one("click", change_box);
-$('.box-2').one("click", change_box);
-$('.box-3').one("click", change_box);
-$('.box-4').one("click", change_box);
-$('.box-5').one("click", change_box);
-$('.box-6').one("click", change_box);
-$('.box-7').one("click", change_box);
-$('.box-8').one("click", change_box);
+// $('.box-0').one("click", change_box);
+// $('.box-1').one("click", change_box);
+// $('.box-2').one("click", change_box);
+// $('.box-3').one("click", change_box);
+// $('.box-4').one("click", change_box);
+// $('.box-5').one("click", change_box);
+// $('.box-6').one("click", change_box);
+// $('.box-7').one("click", change_box);
+// $('.box-8').one("click", change_box);
 
 
 module.exports = {
-
+  change_box,
 }
--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -5,45 +5,48 @@ const clickhandlers = require('./clickhandlers');
 
 
 let check_win = function(board){
-    if(true){
-      for (let i = 0; i < clickhandlers.board.length; i++) {
-        if (clickhandlers.board[i] === ''){
-          return false;
-        }
-      }
+  if(board[0] !== '' && board[0] === board[1] && board[0] === board[2]){
+      console.log('Who wins: ' + board[0]);
+      $('.content').unbind("click");
     }
-    else if(clickhandlers.board[0] === clickhandlers.board[1] && clickhandlers.board[0] === clickhandlers.board[2]){
-      console.log('win');
+    else if (board[3] !== '' && board[3] === board[4] && board[3] === board[5]) {
+      console.log('Who wins: ' + board[3]);
+      $('.content').unbind("click");
     }
-    else if (clickhandlers.board[3]===clickhandlers.board[4] && clickhandlers.board[3] === clickhandlers.board[5]) {
-      console.log('win');
-    }
-    else if (clickhandlers.board[6]===clickhandlers.board[7] && clickhandlers.board[6] === clickhandlers.board[8]){
-      console.log('win');
+    else if (board[6] !== '' && board[6] === board[7] && board[6] === board[8]){
+      console.log('Who wins: ' + board[6]);
+      $('.content').unbind("click");
     }
 
-    else if (clickhandlers.board[0]===clickhandlers.board[3] && clickhandlers.board[0] === clickhandlers.board[6]) {
-      console.log('win');
+    else if (board[0] !== '' && board[0] === board[3] && board[0] === board[6]) {
+      console.log('Who wins: ' + board[0]);
+      $('.content').unbind("click");
     }
-    else if (clickhandlers.board[1]===clickhandlers.board[4] && clickhandlers.board[1] === clickhandlers.board[7]) {
-      console.log('win');
-    }
-
-    else if (clickhandlers.board[2]===clickhandlers.board[5] && clickhandlers.board[2] === clickhandlers.board[8]) {
-      console.log('win');
+    else if (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) {
+      console.log('Who wins: ' + board[1]);
+      $('.content').unbind("click");
     }
 
-    else if (clickhandlers.board[0]===clickhandlers.board[4] && clickhandlers.board[0] === clickhandlers.board[8]) {
-      console.log('win');
+    else if (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) {
+      console.log('Who wins: ' + board[2]);
+      $('.content').unbind("click");
     }
-    else if (clickhandlers.board[2]===clickhandlers.board[4] && clickhandlers.board[2] === clickhandlers.board[6]) {
-      console.log('win');
+
+    else if (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) {
+      console.log('Who wins: ' + board[0]);
+      $('.content').unbind("click");
     }
-    else{
+    else if (board[2] !== '' && board[2] === board[4] && board[2] === board[6]) {
+      console.log('Who wins: ' + board[2]);
+      $('.content').unbind("click");
+    }
+    else if(board.indexOf('') < 0){
       console.log('draw');
     }
+    else{
+      console.log('keep on truckin');
+    }
   };
-
 
 module.exports = {
   check_win,
